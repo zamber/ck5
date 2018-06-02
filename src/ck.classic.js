dc1fb32b3a9ed294325931644285d7b85aeac2d0
Ayhan Akilli <info@ayhan.biz>
2018-06-02 12:56:17 +0200

    Rebuild editors

--- src/ck.classic.js
+++ src/ck.classic.js
@@ -5,23 +5,20 @@
 
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadadapterPlugin from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
+import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import StrikethroughPlugin from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import UnderlinePlugin from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
+import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
 import BlockquotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import EasyimagePlugin from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
 import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
 import ImagecaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImagestylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImagetoolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageuploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload';
-import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
-import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
-import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import TablePlugin from '@ckeditor/ckeditor5-table/src/table';
 
 export default class ClassicEditor extends ClassicEditorBase {}
@@ -29,23 +26,20 @@ export default class ClassicEditor extends ClassicEditorBase {}
 ClassicEditor.build = {
 	plugins: [
 		EssentialsPlugin,
-		UploadadapterPlugin,
 		AutoformatPlugin,
+		HeadingPlugin,
+		ParagraphPlugin,
 		BoldPlugin,
 		ItalicPlugin,
-		StrikethroughPlugin,
 		UnderlinePlugin,
+		LinkPlugin,
+		ListPlugin,
 		BlockquotePlugin,
-		EasyimagePlugin,
-		HeadingPlugin,
 		ImagePlugin,
 		ImagecaptionPlugin,
 		ImagestylePlugin,
 		ImagetoolbarPlugin,
 		ImageuploadPlugin,
-		LinkPlugin,
-		ListPlugin,
-		ParagraphPlugin,
 		TablePlugin
 	],
 	config: {
@@ -58,7 +52,6 @@ ClassicEditor.build = {
 				'bold',
 				'italic',
 				'underline',
-				'strikethrough',
 				'link',
 				'bulletedList',
 				'numberedList',
