e5ed8687deb88596daf3ef53652187c865a96c13
Ayhan Akilli <info@ayhan.biz>
2018-06-04 12:14:40 +0200

    Add widget as dependency

--- src/details.js
+++ src/details.js
@@ -1,9 +1,10 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import Widget from '@ckeditor/ckeditor5-widget/src/widget';
 
 
 export default class Details extends Plugin {
     static get requires() {
-        return [];
+        return [Widget];
     }
 
     static get pluginName() {
