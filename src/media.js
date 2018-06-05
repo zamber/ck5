fad741b91211c493fc0776aacc915eae8a7e640e
Ayhan Akilli <info@ayhan.biz>
2018-06-05 16:12:39 +0200

    Fix + add styles

--- src/media.js
+++ src/media.js
@@ -2,6 +2,7 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import Widget from '@ckeditor/ckeditor5-widget/src/widget';
 import MediaEditing from "./media/mediaediting";
 import MediaTextAlternative from "./mediatextalternative";
+import '../theme/media.css';
 
 export default class Media extends Plugin {
     static get requires() {
--- src/mediacaption.js
+++ src/mediacaption.js
@@ -1,5 +1,6 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import MediaCaptionEditing from "./mediacaption/mediacaptionediting";
+import '../theme/mediacaption.css';
 
 export default class MediaCaption extends Plugin {
     static get requires() {
