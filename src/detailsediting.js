b58873be59521d54f305e71cbbafbf3af0e36639
Ayhan Akilli <info@ayhan.biz>
2018-06-07 20:19:13 +0200

    details::afterInit()

--- src/detailsediting.js
+++ src/detailsediting.js
@@ -16,4 +16,22 @@ export default class DetailsEditing extends Plugin {
         });
         editor.conversion.elementToElement({model: 'details', view: 'details'});
     }
+
+    afterInit() {
+        const editor = this.editor;
+        const command = editor.commands.get('details');
+
+        this.listenTo(this.editor.editing.view.document, 'enter', (evt, data) => {
+            const doc = this.editor.model.document;
+            const positionParent = doc.selection.getLastPosition().parent;
+
+            if (doc.selection.isCollapsed && positionParent.isEmpty && command.value) {
+                this.editor.execute('details');
+                this.editor.editing.view.scrollToTheSelection();
+
+                data.preventDefault();
+                evt.stop();
+            }
+        });
+    }
 }
