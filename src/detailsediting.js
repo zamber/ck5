1706e178f72f55ba9d7ba6b1ebdce01934c74e12
Ayhan Akilli <info@ayhan.biz>
2018-06-07 19:49:03 +0200

    Add conversion 1

--- src/detailsediting.js
+++ src/detailsediting.js
@@ -5,15 +5,14 @@ export default class DetailsEditing extends Plugin {
     init() {
         const editor = this.editor;
         const schema = editor.model.schema;
-        const conversion = editor.conversion;
 
         editor.commands.add('details', new DetailsCommand(editor));
-
         schema.register('details', {
             allowIn: '$root',
             allowWhere: '$block',
             isBlock: true,
             isObject: true
         });
+        editor.conversion.elementToElement({model: 'details', view: 'details'});
     }
 }
