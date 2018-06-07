16324c3d95fa4d07a07e3fbfc1e9737ac3c783e3
Ayhan Akilli <info@ayhan.biz>
2018-06-07 22:16:38 +0200

    Clean up

--- sample/index.js
+++ sample/index.js
@@ -2,21 +2,15 @@
 
 (function (document, InlineEditor) {
     document.addEventListener('DOMContentLoaded', () => {
-        const save = document.querySelector('#save');
-        const rte = document.querySelector('#rte');
         let editor;
 
-        if (!rte) {
-            return;
-        }
-
         InlineEditor
-            .create(rte)
+            .create(document.querySelector('#rte'))
             .then(ed => {
                 editor = ed;
                 console.log(ed);
             });
-        save.addEventListener('click', function () {
+        document.querySelector('#save').addEventListener('click', function () {
             console.log(editor.getData());
         });
     });
