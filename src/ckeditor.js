daa3b6fe7092e4151e76d78377de496a521e3942
Ayhan Akilli <info@ayhan.biz>
2018-06-25 15:00:14 +0200

    Rebuild editor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -15,14 +15,14 @@ import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
 import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
 import BlockquotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import TablePlugin from '@ckeditor/ckeditor5-table/src/table';
+import TabletoolbarPlugin from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import MediaPlugin from '@akilli/ckeditor5-media/src/media';
 import MediacaptionPlugin from '@akilli/ckeditor5-media/src/mediacaption';
 import MediastylePlugin from '@akilli/ckeditor5-media/src/mediastyle';
 import MediatoolbarPlugin from '@akilli/ckeditor5-media/src/mediatoolbar';
 import MediauploadPlugin from '@akilli/ckeditor5-media/src/mediaupload';
 
-export default class InlineEditor extends InlineEditorBase {
-}
+export default class InlineEditor extends InlineEditorBase {}
 
 InlineEditor.build = {
     plugins: [
@@ -37,6 +37,7 @@ InlineEditor.build = {
         ListPlugin,
         BlockquotePlugin,
         TablePlugin,
+        TabletoolbarPlugin,
         MediaPlugin,
         MediacaptionPlugin,
         MediastylePlugin,
@@ -58,9 +59,7 @@ InlineEditor.build = {
                 'numberedList',
                 'blockQuote',
                 'mediaUpload',
-                'insertTable',
-                'insertRowBelow',
-                'insertColumnAfter'
+                'insertTable'
             ]
         },
         heading: {
@@ -96,6 +95,13 @@ InlineEditor.build = {
                 'full',
                 'alignLeft',
                 'alignRight'
+            ]
+        },
+        table: {
+            toolbar: [
+                'tableColumn',
+                'tableRow',
+                'mergeTableCells'
             ]
         }
     }
