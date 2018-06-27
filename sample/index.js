351fbac73ce008c904c1e7f6578dffb0b588dc28
Ayhan Akilli <info@ayhan.biz>
2018-06-27 11:26:48 +0200

    catch()

--- sample/index.js
+++ sample/index.js
@@ -9,6 +9,9 @@
             .then(ed => {
                 editor = ed;
                 console.log(ed);
+            })
+            .catch(error => {
+                console.error(error.stack);
             });
         document.querySelector('#save').addEventListener('click', function () {
             console.log(editor.getData());
