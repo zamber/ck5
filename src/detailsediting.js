e5410262e723e694ad4c5729bd7aba68e3ee7cf8
Ayhan Akilli <info@ayhan.biz>
2019-03-06 15:29:29 +0100

    Clean up

--- src/detailsediting.js
+++ src/detailsediting.js
@@ -4,7 +4,6 @@
 import DetailsCommand from './detailscommand';
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import {toDetailsWidget} from './utils';
-import {toWidgetEditable} from '@ckeditor/ckeditor5-widget/src/utils';
 
 /**
  * Details Editing Plugin
@@ -19,31 +18,16 @@ export default class DetailsEditing extends Plugin {
         const editor = this.editor;
         const schema = editor.model.schema;
         const conversion = editor.conversion;
-        const detailsCfg = {model: 'details', view: 'details'};
-        const summaryCfg = {model: 'detailsSummary', view: 'summary'};
-        const contentCfg = {
-            model: 'detailsContent',
-            view: {
-                name: 'div',
-                attributes: {
-                    class: 'content'
-                }
-            }
-        };
+        const t = editor.t;
 
         editor.commands.add('details', new DetailsCommand(editor));
+
+        // Schema
         schema.register('details', {
-            allowIn: '$root',
             allowWhere: '$block',
             isBlock: true,
             isLimit: true,
             isObject: true
-        });
-        conversion.for('upcast').elementToElement(detailsCfg);
-        conversion.for('dataDowncast').elementToElement(detailsCfg);
-        conversion.for('editingDowncast').elementToElement({
-            model: 'details',
-            view: (element, writer) => toDetailsWidget(writer.createContainerElement('details'), writer)
         });
         schema.register('detailsSummary', {
             allowContentOf: '$block',
@@ -51,23 +35,66 @@ export default class DetailsEditing extends Plugin {
             isBlock: true,
             isLimit: true
         });
-        conversion.for('upcast').elementToElement(summaryCfg);
-        conversion.for('dataDowncast').elementToElement(summaryCfg);
-        conversion.for('editingDowncast').elementToElement({
-            model: 'detailsSummary',
-            view: detailsSummaryEditingDowncast
-        });
         schema.register('detailsContent', {
             allowContentOf: '$root',
             allowIn: 'details',
             isBlock: true,
             isLimit: true
         });
-        conversion.for('upcast').elementToElement(contentCfg);
-        conversion.for('dataDowncast').elementToElement(contentCfg);
+
+        // Element
+        conversion.for('dataDowncast').elementToElement({
+            model: 'details',
+            view: 'details'
+        });
+        conversion.for('editingDowncast').elementToElement({
+            model: 'details',
+            view: (element, writer) => toDetailsWidget(writer.createContainerElement('details'), writer, t('Details'))
+        });
+        conversion.for('upcast').elementToElement({
+            model: 'details',
+            view: 'details'
+        });
+
+        conversion.for('dataDowncast').elementToElement({
+            model: 'detailsSummary',
+            view: 'summary'
+        });
+        conversion.for('editingDowncast').elementToElement({
+            model: 'detailsSummary',
+            view: 'summary'
+        });
+        conversion.for('upcast').elementToElement({
+            model: 'detailsSummary',
+            view: 'summary'
+        });
+
+        conversion.for('dataDowncast').elementToElement({
+            model: 'detailsContent',
+            view: {
+                name: 'div',
+                attributes: {
+                    class: 'content'
+                }
+            }
+        });
         conversion.for('editingDowncast').elementToElement({
             model: 'detailsContent',
-            view: detailsContentEditingDowncast
+            view: {
+                name: 'div',
+                attributes: {
+                    class: 'content'
+                }
+            }
+        });
+        conversion.for('upcast').elementToElement({
+            model: 'detailsContent',
+            view: {
+                name: 'div',
+                attributes: {
+                    class: 'content'
+                }
+            }
         });
     }
 
@@ -93,36 +120,4 @@ export default class DetailsEditing extends Plugin {
             }
         });
     }
-}
-
-/**
- * Downcasts a given model element to a details summary editable
- *
- * @private
- *
- * @param {module:engine/model/element~Element} element
- * @param {module:engine/view/downcastwriter~DowncastWriter} writer
- *
- * @returns {module:engine/view/editableelement~EditableElement}
- */
-function detailsSummaryEditingDowncast(element, writer) {
-    const summary = writer.createContainerElement('summary');
-
-    return toWidgetEditable(summary, writer);
-}
-
-/**
- * Downcasts a given model element to a details content editable
- *
- * @private
- *
- * @param {module:engine/model/element~Element} element
- * @param {module:engine/view/downcastwriter~DowncastWriter} writer
- *
- * @returns {module:engine/view/editableelement~EditableElement}
- */
-function detailsContentEditingDowncast(element, writer) {
-    const content = writer.createContainerElement('div', {class: 'content'});
-
-    return toWidgetEditable(content, writer);
 }
